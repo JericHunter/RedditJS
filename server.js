68aac902300ae783a72c185786054aea5ffaa582
JericHunter <jeric.hunterr@gmail.com>
2020-02-05 15:13:03 -0800

    users can sumbit and save posts

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator')
 
-const exphbs  = require('express-handlebars');
+const app = express();
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Adding body parser
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+
+
+
+
+//Middleware
+const exphbs = require('express-handlebars');
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+//Routes
+app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.get('/', (req, res) => {
+//     res.render('home')
+// });
+
+//Controllers
+require('./controllers/posts.js')(app);
+
+
+app.listen(3000, () => {
+    console.log('Reddit listening on localhost:3000')
+});
+
+module.exports = app;
